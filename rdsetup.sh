119bb8483516bbb84f98631f46b7e4c9609bad03
rwbot <rwbotx@gmail.com>
2018-08-16 02:49:07 +0000

    setup rviz launch file. ROS/environment keeps crashing whenever point cloud is on in RVIZ

---
+++ rdsetup.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# source /home/user/catkin_ws/src/rtabrw/rdsetup.sh
+
+
+alias cdc="cd ~/catkin_ws" #custom cd command that goes to catkin_ws
+alias cdcs="cd ~/catkin_ws/src" #custom cd command that goes to catkin_ws/SRC
+alias cdcr="cdcs; cd rtabrw"
+alias cdcmk="cdc; catkin_make; wait; source devel/setup.bash" #goes to catkin_ws, runs catkin_make, waits until it's made, then sources devel/setup.bash
+alias cddcmk="cdc; rm -rf devel build; catkin_make; wait; source devel/setup.bash" #goes to catkin_ws, deletes build and devel directory runs catkin_make, waits until it's made, then sources devel/setup.bash
+
+alias gitcon="git config --global credential.helper 'cache --timeout=999999'; git config --global user.name 'rwbot'; git config --global user.email 'rwbotx@gmail.com'"
+#
+alias rlu="roslaunch rtabrw udacity_world.launch" #
+alias rla="roslaunch rtabrw amcl.launch" #
+alias rlz="roslaunch rtabrw rviz.launch" #
+alias rlg="roslaunch rtabrw gui.launch" #
+
+# alias delg="rosservice call /gazebo/delete_model \"model_name: 'gurdy'\""
+# alias glg="roslaunch my_gurdy_description spawn_gurdy.launch"
+# alias dglg="delg; wait; glg"
+
+# alias delm="rosservice call /gazebo/delete_model \"model_name: 'mira'\""
+# alias glm="roslaunch my_mira_description spawn_mira.launch"
+# alias dglm="del; wait; glm"
+
+git config --global credential.helper 'cache --timeout=999999' 
+git config --global user.name 'rwbot'
+git config --global user.email 'rwbotx@gmail.com'
+
+source ~/.bashrc
